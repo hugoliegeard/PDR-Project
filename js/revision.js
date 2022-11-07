ff464cde1d530e82923452dbd3101efd69ad030f
Hugo LIEGEARD <hugo@biyn.media>
2022-11-07 12:05:10 +0100

    Session du 07-11-2022
    Correction des formulaires
    Mise en place de Bootstrap
    Révision JS

---
+++ js/revision.js
@@ -0,0 +1,50 @@
+// -- Déclarer une variable
+/*
+
+    |           |
+    |   Prénom  |   <<<<<   Agathe
+    |___________|
+
+    Permet de stocker des informations.
+
+*/
+
+var prenom = "Agathe";
+var dateDeNaissance = "12/01/1995";
+
+console.log ( prenom );
+console.log ( dateDeNaissance );
+
+// ------------------------------------- //
+
+/*
+
+    |            |
+    |   Prénoms  |   <<<<<   Agathe & Hugo
+    |____________|
+
+    Permet de stocker des informations.
+
+*/
+
+var prenoms = [ "Agathe" , "Hugo" ];
+console.log( prenoms );
+
+// var prenom = "Hugo";
+// var nom = "LIEGEARD";
+// var email = "hugo@biyn.media";
+// var tel = "07 83 97 15 15";
+// Plutôt que déclarer plusieurs variables, je peux tous stocker dans une seule.
+
+var contact = [ "Hugo", "LIEGEARD", "hugo@biyn.media", "07 83 97 15 15" ];
+console.log( contact );
+
+// --------------------------------------------------------------------
+
+var animal = "Chien";
+var fleur = "Rose";
+var nourriture = "Croziflette";
+var vetement = "Jean";
+
+console.log( animal );
+console.log( fleur );
